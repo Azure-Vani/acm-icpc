a9491675415b83ba9a7c3108ebe98bb6bbbb629d
Vani <echooatmeal@gmail.com>
2013-07-18 15:09:52 +0800

    23D - Tetragon

--- codeforces/23D.cpp
+++ codeforces/23D.cpp
@@ -1,13 +1,16 @@
 #include <cstdio>
+#include <cassert>
 #include <algorithm>
 #include <cmath>
 #include <vector>
 
 using namespace std;
 
+const double eps = 1e-6;
+
 int Ro[][3] = {{0,1,2},{1,0,2},{2,1,0},{1,2,0},{0,2,1},{2,0,1}};
 
-template<class T> inline int sign(T x) { if (x < 0) return -1; else if (x > 0) return 1; else return 0;}
+template<class T> inline int sign(T x) { if (x < -eps) return -1; else if (x > eps) return 1; else return 0;}
 struct Point {
     double x, y;
     Point():x(0),y(0){}
@@ -52,27 +55,15 @@ inline Point Intersect(const Line& x, const Line& y) {
 }
 
 inline Line getMid(Point a, Point b) {
-	double A = b.y - a.y, B = a.x - b.x, C = ((a.x + b.x) * (b.y - a.y) - (a.y + b.y) * (b.x - a.x)) / 2.0;
+	double A = b.x - a.x, B = b.y - a.y, C = - ((a.x + b.x) * (b.x - a.x) - (a.y + b.y) * (a.y - b.y)) / 2.0;
 	return Line(A, B, C);
-}
-
-inline vector<double> Solve(double A, double B, double C) {
-	vector<double> ret;
-	if (sign(A) == 0) {
-		ret.push_back(-C / B);
-	} else {
-		double delta = B * B - 4 * A * C;
-		ret.push_back(-B + sqrt(delta) / (2 * A));
-		ret.push_back(-B - sqrt(delta) / (2 * A));
-	}
-	return ret;
 }
 
 vector<Point> Ans;
 
 inline bool Cal(void) {
-	if (Ans[0].rotate(Ans[3], Ans[2]) * Ans[0].rotate(Ans[1], Ans[2]) >= 0) return false;
-	if (Ans[1].rotate(Ans[2], Ans[3]) * Ans[1].rotate(Ans[0], Ans[3]) >= 0) return false;
+	if (Ans[0].rotate(Ans[3], Ans[2]) * Ans[0].rotate(Ans[1], Ans[2]) >= -eps) return false;
+	if (Ans[1].rotate(Ans[2], Ans[3]) * Ans[1].rotate(Ans[0], Ans[3]) >= -eps) return false;
 	return true;
 }
 
@@ -80,19 +71,17 @@ inline bool Check(Point a, Point b, Point c) {
 	Line L1 = getMid(a, b);
 	Line L2 = getMid(a, c);
 	Point p0 = Intersect(L1, L2);
-	Point t0 = a - p0, t1 = ((a + b) / 2 - t0).Resize(), t2 = ((a + c) / 2 - t0).Resize();
-	double A = t1 ^ t2, B = a ^ t1, C = t2 ^ a, D = 2 * a.x * a.y, alpha = (t0 ^ t1) / (t0.len() * t1.len()), beta = (t0 ^ t2) / (t0.len() * t1.len());
-	double k = sin(alpha) / sin(beta);
-	vector<double> ret = Solve(A * k, B + C * k, D);
-	for (int i = 0; i < (int)ret.size(); i++) {
-		Point v = t1 * ret[i]; Ans.clear();
-		Ans.push_back(b * 2 - v); Ans.push_back(v); Ans.push_back((a * 2) - v); Ans.push_back((c * 2) - Ans[2]);
-		if (Cal()) return true;
-	}
-	return false;
+	Point t0 = a - p0, t1 = ((a + b) / 2 - p0).Resize(), t2 = ((a + c) / 2 - p0).Resize();
+	double A = t1 ^ t2, B = t0 ^ t1, C = t2 ^ t0, alpha = t1 ^ t0, beta = t0 ^ t2;
+	double k = alpha / beta;
+	double ret = (B + C * k) / (-A * k);
+	Point v = t1 * ret + p0; Ans.clear();
+	Ans.push_back(b * 2 - v); Ans.push_back(v); Ans.push_back((a * 2) - v); Ans.push_back((c * 2) - Ans[2]);
+	return Cal();
 }
 
 int main(void) {
+	freopen("in", "r", stdin);
 	int kase; scanf("%d", &kase); for (int _ = 1; _ <= kase; _++) {
 		Point a[3]; for (int i = 0; i < 3; i++) scanf("%lf%lf", &a[i].x, &a[i].y);
 		if (a[0].rotate(a[1], a[2]) == 0) {
@@ -107,7 +96,8 @@ int main(void) {
 			}
 			if (ok) {
 				puts("YES");
-				for (int i = 0; i < 4; i++) printf("%lf %lf\n", Ans[i].x, Ans[i].y);
+				for (int i = 0; i < 4; i++) printf("%.9lf %.9lf ", Ans[i].x, Ans[i].y);
+				puts("");
 			} else puts("NO\n");
 		}
 	}
